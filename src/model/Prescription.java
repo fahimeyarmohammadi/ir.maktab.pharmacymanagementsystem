658ed5fb3f49b4d8bd9d6143df75c977a89d174e
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-19 14:26:28 +0430

    add getter and setter to Prescription  class

--- src/model/Prescription.java
+++ src/model/Prescription.java
@@ -6,4 +6,30 @@ public class Prescription {
     private int id;
     private int userId;
     private List<Medicine>medicineList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public List<Medicine> getMedicineList() {
+        return medicineList;
+    }
+
+    public void setMedicineList(List<Medicine> medicineList) {
+        this.medicineList = medicineList;
+    }
 }
+
+
