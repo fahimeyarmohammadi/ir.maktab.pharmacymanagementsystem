56dcfe393d5a3cfafb500c227e9240ef3c90e5db
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-19 12:40:49 +0430

    update GetConnectionClass

--- src/repository/GetConnection.java
+++ src/repository/GetConnection.java
@@ -1,2 +1,11 @@
-package repository;public class GetConnection {
+package repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GetConnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/maktab", "postgres", "Fy4965285");
+    }
 }
