de14f1e51694a2e553212c84e98f0ff32a86e792
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-19 14:28:21 +0430

    add insert prescription method to PrescriptionRepository class

--- src/repository/PrescriptionRepository.java
+++ src/repository/PrescriptionRepository.java
@@ -1,9 +1,20 @@
 package repository;
 
+import model.Prescription;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class PrescriptionRepository {
+public void insertPrescription(Prescription prescription) throws SQLException {
+    Connection connection=GetConnection.getConnection();
+    PreparedStatement preparedStatement=connection.prepareStatement("insert into prescription_tbl " +
+            " (id , user_id ,medicine_id) values (?,?,?)");
+    preparedStatement.setInt(1,prescription.getId());
+    preparedStatement.setInt(1,prescription.getUserId());
+    preparedStatement.setInt(1,prescription.getMedicineList().get(0).getId());
 
+    preparedStatement.executeUpdate();
+}
 }
