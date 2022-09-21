d7968a229cc06bd1d0b112c1d20379fa21120bac
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-21 18:32:11 +0430

    add getPrescription , getMedicineForEachPrescription , cofirmprescription, getExistMedicin

--- src/repository/PrescriptionRepository.java
+++ src/repository/PrescriptionRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import model.Medicine;
 import model.Prescription;
 
 import java.sql.Connection;
@@ -67,15 +68,58 @@ public void deleteAMedicineFromPrescription(int id,String medicineName) throws S
 
 public List<Prescription> getPrescription() throws SQLException {
     Connection connection=GetConnection.getConnection();
-    PreparedStatement preparedStatement=connection.prepareStatement("select * from prescription_tbl" +
-            " inner join medicinesOfPrescription_tbl on prescription_tbl.id=medicinesOfPrescription_tbl.prescription_id ");
+    PreparedStatement preparedStatement=connection.prepareStatement("select * from prescription_tbl");
     ResultSet resultSet=preparedStatement.executeQuery();
     List<Prescription> list=new ArrayList<>();
     while(resultSet.next()){
         Prescription prescription=new Prescription(
                 resultSet.getInt(1),
+                resultSet.getBoolean(2),
+                resultSet.getInt(3),
+                resultSet.getString(4)
         );
     }
     return list;
+}
+
+public List<Medicine>getMedicineForEachPrescription(int id) throws SQLException {
+    Connection connection=GetConnection.getConnection();
+    PreparedStatement preparedStatement=connection.prepareStatement("select * from medicine_tbl inner join" +
+            "  medicineOfPrescription_tbl on medicine_tbl.name=medivineOfPrescription_tbl.medicineName where prescription_id=?");
+    preparedStatement.setInt(1,id);
+
+    ResultSet resultSet=preparedStatement.executeQuery();
+    List<Medicine> medicineList=new ArrayList<>();
+    while(resultSet.next()){
+        Medicine medicine=new Medicine(
+                resultSet.getString(1)
+        );
+        medicineList.add(medicine);
+    }
+    return medicineList;
+}
+
+public void confirmPrescription(int id ) throws SQLException {
+    Connection connection=GetConnection.getConnection();
+    PreparedStatement preparedStatement=connection.prepareStatement("update prescription_tbl" +
+            " set isconfirmed=? where id=?");
+    preparedStatement.setBoolean(1,true);
+    preparedStatement.setInt(2,id);
+    preparedStatement.executeUpdate();
+}
+public List<Medicine> getExistMedicine(int id) throws SQLException {
+    Connection connection=GetConnection.getConnection();
+    PreparedStatement preparedStatement=connection.prepareStatement("select name , isExist from medicine_tbl" +
+            "inner join medicineOfPrescription_tbl where" +
+            " medicineOfPrescription_tbl.prescription_id=?");
+    ResultSet resultSet=preparedStatement.executeQuery();
+    List<Medicine>medicineList=new ArrayList<>();
+    while(resultSet.next()){
+        if(resultSet.getBoolean(2)){
+            Medicine medicine=new Medicine(resultSet.getString(1));
+            medicineList.add(medicine);
+        }
+    }
+        return medicineList;
 }
 }
