2907d6e2d7bd4f5eabb74a425b0143e824a51b82
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-21 14:08:40 +0430

    add getPriscription , deletAMedicineFromPrescription , deleteAllMedicineFromPrescription methodes to Prescription table

--- src/repository/PrescriptionRepository.java
+++ src/repository/PrescriptionRepository.java
@@ -13,26 +13,24 @@ public class PrescriptionRepository {
 public void insertPrescription(Prescription prescription) throws SQLException {
     Connection connection=GetConnection.getConnection();
     PreparedStatement preparedStatement=connection.prepareStatement("insert into prescription_tbl " +
-            " (user_natinalcode ,numberofmedicine ,medicine1 ,medicine2 ," +
-            "medicine3 ,medicine4 ,medicine_5 ," +
-            "medicine6 ,medicine7 ,medicine8," +
-            " medicine9 ,medicine10 ,isconfirmed ) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            " (user_natinalcode ,numberofmedicine, totalPrice ,isconfirmed ) values (?,?,?,?)");
 
     preparedStatement.setString(1,prescription.getUserNationalCode());
     preparedStatement.setInt(2,prescription.getNumberOfMedicine());
-    preparedStatement.setString(3,prescription.getMedicineList().get(0).getName());
-    preparedStatement.setString(4,prescription.getMedicineList().get(1).getName());
-    preparedStatement.setString(5,prescription.getMedicineList().get(2).getName());
-    preparedStatement.setString(6,prescription.getMedicineList().get(3).getName());
-    preparedStatement.setString(7,prescription.getMedicineList().get(4).getName());
-    preparedStatement.setString(8,prescription.getMedicineList().get(5).getName());
-    preparedStatement.setString(9,prescription.getMedicineList().get(6).getName());
-    preparedStatement.setString(10,prescription.getMedicineList().get(7).getName());
-    preparedStatement.setString(11,prescription.getMedicineList().get(8).getName());
-    preparedStatement.setString(12,prescription.getMedicineList().get(9).getName());
-    preparedStatement.setBoolean(13,prescription.isConfirmed());
+    preparedStatement.setDouble(3,prescription.getTotalPrice());
+    preparedStatement.setBoolean(4,prescription.isConfirmed());
 
     preparedStatement.executeUpdate();
+}
+public void insertMedicineToPrescription(int prescriptionId, String medicineName) throws SQLException {
+    Connection connection=GetConnection.getConnection();
+    PreparedStatement preparedStatement=connection.prepareStatement( "insert into medicinesOfPrescription_tbl" +
+            "(prescription_id , medicineName ) values (?,?)");
+    preparedStatement.setInt(1,prescriptionId);
+    preparedStatement.setString(2,medicineName);
+
+    preparedStatement.executeUpdate();
+
 }
 
 public void getConfirmedPrescription(int id) throws SQLException {
@@ -50,16 +48,27 @@ public void deletePrescription(int id) throws SQLException {
     preparedStatement.executeUpdate();
 
 }
-
-public void addMedicineFromPrescription(int id,int medicineId) throws SQLException {
+public void deleteAllMedicineFromPrescription(int id) throws SQLException {
     Connection connection=GetConnection.getConnection();
-    PreparedStatement preparedStatement=connection.prepareStatement("update prescription_tbl" +
-            " set   where id=?");
+    PreparedStatement preparedStatement=connection.prepareStatement( "delete from medicinesOfPrescription_tbl" +
+            " where prescription_id=?");
+    preparedStatement.setInt(1,id);
+    preparedStatement.executeUpdate();
+}
+
+public void deleteAMedicineFromPrescription(int id,String medicineName) throws SQLException {
+    Connection connection=GetConnection.getConnection();
+    PreparedStatement preparedStatement=connection.prepareStatement( "delete from medicinesOfPrescription_tbl" +
+            " where prescription_id=? and medicineName=?");
+    preparedStatement.setInt(1,id);
+    preparedStatement.setString(2,medicineName);
+    preparedStatement.executeUpdate();
 }
 
 public List<Prescription> getPrescription() throws SQLException {
     Connection connection=GetConnection.getConnection();
-    PreparedStatement preparedStatement=connection.prepareStatement("select * from prescription_tbl ");
+    PreparedStatement preparedStatement=connection.prepareStatement("select * from prescription_tbl" +
+            " inner join medicinesOfPrescription_tbl on prescription_tbl.id=medicinesOfPrescription_tbl.prescription_id ");
     ResultSet resultSet=preparedStatement.executeQuery();
     List<Prescription> list=new ArrayList<>();
     while(resultSet.next()){
@@ -68,6 +77,5 @@ public List<Prescription> getPrescription() throws SQLException {
         );
     }
     return list;
-
 }
 }
