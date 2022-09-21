7460a12375b18bf0e846fb7cb9772639ceb9a7bd
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-21 18:28:02 +0430

    update MedicineRepository Class

--- src/repository/MedicineRepository.java
+++ src/repository/MedicineRepository.java
@@ -1,4 +1,4 @@
-package repository;
+gipackage repository;
 
 import model.Medicine;
 
@@ -6,16 +6,26 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MedicineRepository {
     public List<Medicine> getPriceOfMedicine() throws SQLException {
         Connection connection=GetConnection.getConnection();
-        PreparedStatement preparedStatement=connection.prepareStatement("select * from prescription_tbl" +
-                " inner join medicinesOfPrescription_tbl on prescription_tbl.id=medicinesOfPrescription_tbl.prescription_id" +
-                " inner join medicine_tbl on medicinesOfPrescription_tbl.medicineNme=medicine_tbl.name where" +
+        PreparedStatement preparedStatement=connection.prepareStatement("select name , price from medicine_tbl" +
+                " inner join medicinesOfPrescription_tbl on medicine_tbl.name=medicinesOfPrescription_tbl.medicineName" +
+                " inner join prescription_tbl on medicinesOfPrescription_tbl.prescription_id=prescription_tbl.id where" +
                 " isconfirmed=?");
         preparedStatement.setBoolean(1,true);
         ResultSet resultSet=preparedStatement.executeQuery();
+        List<Medicine>medicineList=new ArrayList<>();
+        while (resultSet.next()){
+            Medicine medicine=new Medicine(
+                    resultSet.getString(0),
+                    resultSet.getDouble(1)
+            );
+            medicineList.add(medicine);
+        }
+        return medicineList;
     }
 }
