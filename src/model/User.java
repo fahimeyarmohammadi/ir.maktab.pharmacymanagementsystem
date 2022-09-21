a00d49f719856f12250810a8db6a6b896cc5e6ce
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-21 04:57:22 +0430

    update User class

--- src/model/User.java
+++ src/model/User.java
@@ -2,6 +2,27 @@ package model;
 
 public class User {
     private String userName;
-    private String password;
+    private String nationalCode;
+
+    public User(String userName, String nationalCode) {
+        this.userName = userName;
+        this.nationalCode = nationalCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String  getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
 
