0a96d26db5bbea42f2622972d2577cac81d2a6fe
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-21 04:59:11 +0430

    add userSignIn method and insertUser method to userRepository

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,2 +1,35 @@
-package repository;public class UserRepository {
+package repository;
+
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRepository {
+    public void insertUser(User user) throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into User_tbl" +
+                " (username , nationalcode) values (?,?)");
+        preparedStatement.setString(1, user.getUserName());
+        preparedStatement.setString(2, user.getNationalCode());
+
+        preparedStatement.executeUpdate();
+    }
+
+    public boolean UserSignIn(String username, String password) throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from User_tbl" +
+                " where username=? , password=?");
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next())
+            return true;
+        else
+            return false;
+
+    }
 }
